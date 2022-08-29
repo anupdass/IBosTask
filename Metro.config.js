09fedff558aa31cf4c5da59ecf8280decc25fc09
anupdass <anupdas633@gmail.com>
2022-08-29 15:22:49 +0600

    l

--- Metro.config.js
+++ Metro.config.js
@@ -1,16 +1,15 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+// extra config is needed to enable `react-native-svg-transformer`
 module.exports = (async () => {
     const {
         resolver: { sourceExts, assetExts },
-    } = await getDefaultConfig();
+    } = await getDefaultConfig(__dirname);
     return {
         transformer: {
-            getTransformOptions: async () => ({
-                transform: {
-                    experimentalImportSupport: false,
-                    inlineRequires: true,
-                },
-            }),
             babelTransformerPath: require.resolve('react-native-svg-transformer'),
+            assetPlugins: ['expo-asset/tools/hashAssetFiles'],
         },
         resolver: {
             assetExts: assetExts.filter((ext) => ext !== 'svg'),
